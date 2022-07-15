0ecc6ca22394d1bbe8f40ca559c6f6f4a4588211
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-07-15 16:46:27 -0500

    Added iframe support!

--- disable-autoscroll-webextension/index.js
+++ disable-autoscroll-webextension/index.js
@@ -1,6 +1,10 @@
-document.addEventListener('mousedown', function(mouseEvent) {
-  if (mouseEvent.button != 1)
-    return;
+const MIDDLE_MOUSE_BUTTON_ID = 1;
+
+document.addEventListener('mousedown', function disableAutoscroll(mouseEvent) {
+  // Let other mouse button events propagate:
+  if (mouseEvent.button != MIDDLE_MOUSE_BUTTON_ID)
+    return; 
+  // Otherwise cancel middle mouse button event:
   mouseEvent.preventDefault();
   mouseEvent.stopPropagation();
 });
